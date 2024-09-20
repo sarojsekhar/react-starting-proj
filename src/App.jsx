d42731f47c2a06d55e929cc9df6ba8347ef4331b
sarojsekhar <sarojsekhar25@gmail.com>
2024-09-20 17:16:17 +0530

    - commit from ofc system

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,32 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
-
+import { EXAMPLES } from './data.js';
 
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button.');
+  const [ selectedTopic, setSelectedTopic ] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     // console.log(selectedTopic);
 
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -41,7 +57,37 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+
+          {/* Below code is 1st Alternative using ternary Operator */}
+          {/* {!selectedTopic ? <p>Please select a topic.</p> : null}
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          ) : null} */}
+
+          {/* Below code is 2nd Alternative using && operator */}
+          {/* {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div>
+          )} */}
+
+          {/*  Below code is the 3rd Alternative using dynamic value to get the contents */}
+          {tabContent}
         </section>
       </main>
     </div>
